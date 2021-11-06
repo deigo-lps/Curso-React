bdb6b751b8247fb5810976be3fe0dcaa5041c5cc
deigo-lps <diego.lopes0909@gmail.com>
2021-11-05 21:02:45 -0300

    useState

--- src/components/Expenses/ExpenseItem/index.js
+++ src/components/Expenses/ExpenseItem/index.js
@@ -1,16 +1,28 @@
+import React, { useState } from 'react';
+
 import Card from "../../UI/Card"
 import Date from "../../UI/Date"
 import './ExpenseItem.scss'
 export default function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title)
+
+  const clickHandler = () => {
+    setTitle("Clicked")
+  }
+
+  // setTimeout(()=>{setTitle("Clicked")},3000)
+
   return (
     <Card className="expense-item">
       <Date>
         {props.date}
       </Date>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
