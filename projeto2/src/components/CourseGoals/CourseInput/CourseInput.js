ffdd7ab3ae26c81d7ca5df40dcf378efe9b5123b
deigo-lps <diego.lopes0909@gmail.com>
2021-11-09 15:27:56 -0300

    css modules

--- projeto2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ projeto2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,42 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.scss";
-import styled from "styled-components";
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  }
-
-  input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-  }
-
-  input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-  }
-
-  &.invalid {
-  input {
-    border-color: red;
-    background-color: rgba(255, 0, 0, 0.26);
-  }
-  label{
-    color:var(--red);
-  }
-`;
+import styles from "./CourseInput.module.scss";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -61,15 +26,14 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* <div className={`form-control ${!isValid ? "invalid" : ""}`}> */}
-      <FormControl className={!isValid && 'invalid'}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input
           type="text"
           value={enteredValue}
           onChange={goalInputChangeHandler}
         />
-      </FormControl>
-      {/* </div> */}
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
