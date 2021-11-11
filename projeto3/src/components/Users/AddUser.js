30c2b0d7721e124d0b4fc4fbbd226ba84dd4b546
deigo-lps <diego.lopes0909@gmail.com>
2021-11-11 12:30:12 -0300

    ref

--- projeto3/src/components/Users/AddUser.js
+++ projeto3/src/components/Users/AddUser.js
@@ -1,59 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.scss";
 import Button from "../UI/Button";
-import ErrorModal from "../UI/ErrorModal"
+import ErrorModal from "../UI/ErrorModal";
 export default function AddUser(props) {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [error,setError] = useState(undefined);
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
-  const handleName = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleAge = (event) => {
-    setAge(event.target.value);
-  };
+  const [error, setError] = useState(undefined);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(name.trim().length === 0 || age.length === 0){
-      setError({title:"Invalid input.",message:"Name or age invalid (must not be blank)."})
-      return
+    const name = nameInputRef.current.value;
+    const age = ageInputRef.current.value;
+    if (name.trim().length === 0 || age.length === 0) {
+      setError({
+        title: "Invalid input.",
+        message: "Name or age invalid (must not be blank).",
+      });
+      return;
     }
-    if(+age<=0){
-      setError({title:"Invalid age.",message:"Age must be greater than 0."})
-      return
+    if (+age <= 0) {
+      setError({
+        title: "Invalid age.",
+        message: "Age must be greater than 0.",
+      });
+      return;
     }
-    props.onAddUser({"name": name,"age": age, "id": Math.random().toString()})
-    setName("");
-    setAge("");
+    props.onAddUser({ name: name, age: age, id: Math.random().toString() });
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+    nameInputRef.current.focus();
   };
 
-  const handleError = ()=>{
-    setError(undefined)
-  }
+  const handleError = () => {
+    setError(undefined);
+  };
 
   return (
     <Card>
-      {error && <ErrorModal title={error.title} message={error.message} close={handleError}></ErrorModal>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          close={handleError}
+        ></ErrorModal>
+      )}
       <form onSubmit={handleSubmit} className={styles.form}>
         <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          value={name}
-          type="text"
-          onChange={handleName}
-        ></input>
+        <input id="username" type="text" ref={nameInputRef}></input>
         <label htmlFor="age">Age</label>
-        <input
-          id="age"
-          value={age}
-          type="number"
-          step="1"
-          onChange={handleAge}
-        ></input>
+        <input id="age" type="number" step="1" ref={ageInputRef}></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
