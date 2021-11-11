20952255974609b1c8b8d02f2e4b5ac1f729e289
deigo-lps <diego.lopes0909@gmail.com>
2021-11-11 00:29:26 -0300

    portal

--- projeto3/src/components/UI/ErrorModal.js
+++ projeto3/src/components/UI/ErrorModal.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import Button from './Button'
-import Card from './Card'
-import styles from './ErrorModal.module.scss'
-export default function ErrorModal(props){
-  return(
-    <>
-    <div className={styles.backdrop}/>
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./Button";
+import Card from "./Card";
+import styles from "./ErrorModal.module.scss";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.close} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
     <Card className={styles.modal}>
       <h2 className={styles.modal__title}>{props.title}</h2>
       <p className={styles.modal__message}>{props.message}</p>
       <Button onClick={props.close}>Okay</Button>
     </Card>
+  );
+};
+
+export default function ErrorModal(props) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop close={props.close} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          close={props.close}
+          message={props.message}
+          title={props.title}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
-  )
+  );
 }
