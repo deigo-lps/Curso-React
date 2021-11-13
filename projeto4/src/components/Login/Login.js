449fd3b2120ffda17e0c2b90b54d4db5af4448a0
deigo-lps <diego.lopes0909@gmail.com>
2021-11-13 17:58:03 -0300

    cleanup function

--- projeto4/src/components/Login/Login.js
+++ projeto4/src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(()=>{
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    },500);
+    //cleanup function
+    //runs before every new side-effect function execution
+    //and before the component is removed
+    return ()=>{
+      clearTimeout(identifier);
+    };
   },[enteredEmail,enteredPassword]);
 
   const emailChangeHandler = (event) => {
