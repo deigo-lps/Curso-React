d658401ccabbc2166bc302d5c14b14fa3841ccc9
deigo-lps <diego.lopes0909@gmail.com>
2021-12-18 21:25:27 -0300

    useContext

--- projeto4/src/App.js
+++ projeto4/src/App.js
@@ -29,9 +29,11 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}>
+      <MainHeader/>
       <main>
+        {/* como loginHandler e logoutHandler serão usados diretamente pelos componentes
+        tudo bem mandar por props. */}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
