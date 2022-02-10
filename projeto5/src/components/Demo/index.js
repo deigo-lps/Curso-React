7762a03cb89f020eaa86f214597a271048b292cd
deigo-lps <diego.lopes0909@gmail.com>
2022-02-10 10:46:29 -0300

    memo

--- projeto5/src/components/Demo/index.js
+++ projeto5/src/components/Demo/index.js
@@ -6,5 +6,10 @@ const Demo = (props) =>{
     <p>{props.show ? 'this is new' : ''}</p>
   )
 }
-
-export default Demo;
+// para esse componente react deve checar os valores dos props
+// e comparar com os anteriores.
+// só vai reavaliar se os props mudaram.
+// é um trabalho a mais pro react que tem que armazenar os valores do props
+// e dps comparar dnv ent tem q ta vendo se vale a pena.
+// bom usar no topo da arvore de componentes que ja evita reavaliar os abaixo
+export default React.memo(Demo);
