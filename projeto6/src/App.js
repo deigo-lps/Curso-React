70c5ce57f236c47776b7f371e176196616511101
deigo-lps <diego.lopes0909@gmail.com>
2022-02-14 12:03:12 -0300

    get

--- projeto6/src/App.js
+++ projeto6/src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import MoviesList from './components/Movieslist/MoviesList';
 import './style.scss'
-const dummyMovies = [
-  {
-    id: 1,
-    title: 'Some Dummy Movie',
-    openingText: 'This is the opening text of the movie',
-    releaseDate: '2021-05-18',
-  },
-  {
-    id: 2,
-    title: 'Some Dummy Movie 2',
-    openingText: 'This is the second opening text of the movie',
-    releaseDate: '2021-05-19',
-  },
-];
+
 function App() {
+  const[movies,setMovies] = useState([])
+
+  const fetchMoviesHandler = () =>{
+    fetch('https://swapi.dev/api/films').then(response => {
+      return response.json();
+    }).then(data =>{
+      const transformedMovies = data.results.map((movieData)=>{
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      });
+      setMovies(transformedMovies);
+      console.log(data.results);
+    });
+  }
+
+
   return (
     <>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies}/>
+        <MoviesList movies={movies}/>
       </section>
     </>
   );
