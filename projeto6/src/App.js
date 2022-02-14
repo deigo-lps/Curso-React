8225bb8bf5cce8dee45126851959968b2e132688
deigo-lps <diego.lopes0909@gmail.com>
2022-02-14 12:31:46 -0300

    loading

--- projeto6/src/App.js
+++ projeto6/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import MoviesList from "./components/Movieslist/MoviesList";
 import "./style.scss";
+import loading from "./components/img/loading.svg";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setisLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
-    
+
     const transformedMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
@@ -18,16 +21,26 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setisLoading(false);
     console.log(data.results);
   }
 
   return (
     <>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      <section class="main">
+        {!isLoading ? (
+          <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        ) : (
+          <img class="loading" src={loading}></img>
+        )}
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {
+          movies.length === 0 ?
+          <p style={{color: "white",fontSize: "1.5rem"}}>No movies were found.</p>
+          :
+          <MoviesList movies={movies} />
+        }
       </section>
     </>
   );
