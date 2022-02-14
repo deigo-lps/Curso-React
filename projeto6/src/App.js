675c7653be2b05df9f4bfaa81f3300bf7138aff7
deigo-lps <diego.lopes0909@gmail.com>
2022-02-14 12:06:58 -0300

    async await

--- projeto6/src/App.js
+++ projeto6/src/App.js
@@ -1,27 +1,25 @@
-import React,{useState} from 'react';
-import MoviesList from './components/Movieslist/MoviesList';
-import './style.scss'
+import React, { useState } from "react";
+import MoviesList from "./components/Movieslist/MoviesList";
+import "./style.scss";
 
 function App() {
-  const[movies,setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () =>{
-    fetch('https://swapi.dev/api/films').then(response => {
-      return response.json();
-    }).then(data =>{
-      const transformedMovies = data.results.map((movieData)=>{
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      });
-      setMovies(transformedMovies);
-      console.log(data.results);
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
     });
+    setMovies(transformedMovies);
+    console.log(data.results);
   }
-
 
   return (
     <>
@@ -29,7 +27,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies}/>
+        <MoviesList movies={movies} />
       </section>
     </>
   );
