46a83a54dc7b539d2f2369c185380dba986785fb
deigo-lps <diego.lopes0909@gmail.com>
2022-02-15 13:43:02 -0300

    adicionando fetchTasks como uma dependencia do useEffect

--- projeto8/src/App.js
+++ projeto8/src/App.js
@@ -6,24 +6,25 @@ import NewTask from "./components/NewTask/NewTask";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const [isLoading, error, fetchTasks] = useHttp(
-    { url: "https://curso-react-f70ea-default-rtdb.firebaseio.com/tasks.json" },
-    transformTasks
-  );
+  const [isLoading, error, fetchTasks] = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: "https://curso-react-f70ea-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
