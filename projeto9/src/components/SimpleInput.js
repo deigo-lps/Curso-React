7e3616b752094a28959770dade270a8d3b7aef66
deigo-lps <diego.lopes0909@gmail.com>
2022-02-16 11:10:27 -0300

    validaçao form

--- projeto9/src/components/SimpleInput.js
+++ projeto9/src/components/SimpleInput.js
@@ -1,9 +1,38 @@
+import React, { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+
+  const [enteredName, setEnteredName] = useState("");
+  const [valid, setValid] = useState(true);
+  const inputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setValid(false);
+      return;
+    }
+    setValid(true);
+
+    alert(enteredName);
+    alert(nameInputRef.current.value);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={handleSubmit}>
+      <div className={valid ? 'form-control' : 'form-control invalid'}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          onChange={inputHandler}
+        />
+        {!valid && <p className="error-text">Name must not be blank.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
