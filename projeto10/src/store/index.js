d80789e23eb4d578655eb5247ed3e8fbd003da00
deigo-lps <diego.lopes0909@gmail.com>
2022-02-21 13:15:06 -0300

    linka ao componente

--- projeto10/src/store/index.js
+++ projeto10/src/store/index.js
@@ -1,4 +1,4 @@
-import redux from "redux";
+import {createStore} from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
   switch (action) {
@@ -11,6 +11,6 @@ const counterReducer = (state = { counter: 0 }, action) => {
   }
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
