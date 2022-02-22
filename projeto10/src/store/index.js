8e3ac831f0d3fd72a29d077160e54eed6dc65d14
deigo-lps <diego.lopes0909@gmail.com>
2022-02-22 17:08:00 -0300

    splitting code

--- projeto10/src/store/index.js
+++ projeto10/src/store/index.js
@@ -1,43 +1,9 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: { counter: 0, showCounter: true },
-  reducers: {
-    increment(state) {
-      state.counter++; //só pode mudar assim por aqui.
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter += action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: {
-    isAuthenticated: false,
-  },
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 export default store;
