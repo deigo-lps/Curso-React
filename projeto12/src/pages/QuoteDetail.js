89edfb6cd73b59675b282a7072f2316c739a6246
deigo-lps <diego.lopes0909@gmail.com>
2022-04-20 11:13:04 -0300

    more nested routes

--- projeto12/src/pages/QuoteDetail.js
+++ projeto12/src/pages/QuoteDetail.js
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Comments from "../components/comments/Comments";
-import HighlightedQuote from "../components/quotes/HighlightedQuote"
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Card from "../components/UI/Card";
 const DUMMY_QUOTES = [
   {
@@ -16,23 +17,46 @@ const DUMMY_QUOTES = [
   },
 ];
 
+const CommentButton = (props) => {
+  return (
+    <Route path={props.routePath} exact>
+      <div className="centered">
+        <Link className="btn--flat" to={props.path}>
+          {props.children}
+        </Link>
+      </div>
+    </Route>
+  );
+};
 
 const QuoteDetail = () => {
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote=>quote.id===params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if(!quote){
-    return(
+  if (!quote) {
+    return (
       <Card>
         <p>No Quotes.</p>
       </Card>
-    )
+    );
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <CommentButton
+        routePath={`/quotes/${params.quoteId}`}
+        path={`/quotes/${params.quoteId}/comments`}
+      >
+        Load Comments
+      </CommentButton>
+      <CommentButton
+        routePath={`/quotes/${params.quoteId}/comments`}
+        path={`/quotes/${params.quoteId}`}
+      >
+        Hide Comments
+      </CommentButton>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
