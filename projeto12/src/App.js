39458a26524a457e018ec24f7ed1b00cc47fea5a
deigo-lps <diego.lopes0909@gmail.com>
2022-06-21 16:38:41 -0300

    lazy loading

--- projeto12/src/App.js
+++ projeto12/src/App.js
@@ -1,30 +1,36 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import MainNavigation from "./components/layout/MainNavigation";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
-import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import Layout from "./components/layout/Layout";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes'/>
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes/>
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail/>
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote/>
-        </Route>
-        <Route path='*'>
-          <NotFound/>
-        </Route>
-      </Switch>
+      <Suspense fallback={<div className="centered"><LoadingSpinner/></div>}>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
